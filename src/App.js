6f3d47816bad228a5ae5667b1d28aa912db2854e
abaspoor@hotmail.com <abaspoor@hotmail.com>
2023-07-19 15:38:55 +0330

    ok

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Main from "./components/main";
 import Sidebar from "./components/sidebar";
 import { ThemeProvider} from "@mui/material/styles";
 import theme from "./theme";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
 
   return (
       <ThemeProvider theme={theme}>
     <div className="App">
-      <header>
-          <Header/>
-          <div className="general-content">
-              <Sidebar/>
-              <Main/>
-          </div>
-      </header>
+        <Router>
+            <header>
+              <Header/>
+              <div className="general-content">
+                  <Sidebar/>
+                  <Main/>
+              </div>
+        </header>
+        </Router>
     </div>
       </ThemeProvider>
   );
--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import GroupList from "./group-list";
 
 function Main(){
     return(
         <div className="main">
-            <GroupList/>
+            <Routes>
+                <Route exact path={'/'} element={<GroupList/>}></Route>
+                <Route path={'/details/:id'} element={<h1>Details</h1>}></Route>
+            </Routes>
         </div>
     );
 }
