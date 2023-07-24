a3db1d30541b721cb51695f1050edc6cccb1af7e
Abasp <abaspoor@hotmail.com>
2023-07-24 10:57:21 +0330

    work

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,24 +1,24 @@
- import React, {useState} from 'react';
+import React, {useState,useContext} from 'react';
 import { Button } from '@mui/material';
 import { InputWithIcon } from './textfield';
 import { auth } from '../services/user-services'
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from '../hooks/useAuth';
 
  function Sidebar(){
     const [UserName,PassWord, loginField] = InputWithIcon();
-    // const { authData, setAuth } = useAuth();
-     const [ authData, setAuth ] = useState('');
+    const { authData, setAuth } = useAuth();
     const Handler = async e =>{
         e.preventDefault();
         const data = await auth({username: UserName, password:PassWord});
         setAuth(data);
+        console.log(authData);
     }
     return(
         <div className="sidebar">
             {authData && <p>{authData.token}</p>}
 			<form onSubmit={Handler}>
                 {loginField}
-            <Button color="primary" variant="contained" type='submit'>
+            <Button color="primary" type="submit"	variant="contained" >
                 Submit
             </Button>
             </form>
