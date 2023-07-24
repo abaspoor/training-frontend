dae5d5136baf8fa1e7f3169a5cb9131d7258e0d8
Abasp <abaspoor@hotmail.com>
2023-07-24 19:22:19 +0330

    watch 6 7 8 again

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,15 +1,21 @@
 import React, {useState,useContext} from 'react';
 import { Button } from '@mui/material';
-import { InputWithIcon } from './textfield';
+// import { InputWithIcon } from './textfield';
 import { auth } from '../services/user-services'
 import { useAuth } from '../hooks/useAuth';
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import PasswordIcon from "@mui/icons-material/Password";
+import Box from "@mui/material/Box";
 
  function Sidebar(){
-    const [UserName,PassWord, loginField] = InputWithIcon();
-    const { authData, setAuth } = useAuth();
+     const [username, setUsername] = useState('');
+     const [password, setPassword] = useState('');
+     const { authData, setAuth } = useAuth();
     const Handler = async e =>{
         e.preventDefault();
-        const data = await auth({username: UserName, password:PassWord});
+        const data = await auth({username, password});
         setAuth(data);
         console.log(authData);
     }
@@ -17,7 +23,35 @@ import { useAuth } from '../hooks/useAuth';
         <div className="sidebar">
             {authData && <p>{authData.token}</p>}
 			<form onSubmit={Handler}>
-                {loginField}
+                <Box sx={{'& > :not(style)': {m: 1}}}>
+                    <TextField
+                        id="username"
+                        label="UserName"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <AccountCircle color="primary"/>
+                                </InputAdornment>
+                            ),
+                        }}
+                        variant="standard"
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <TextField
+                        id="password"
+                        label="Password"
+                        type={"password"}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <PasswordIcon color="primary"/>
+                                </InputAdornment>
+                            ),
+                        }}
+                        variant="standard"
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </Box>
             <Button color="primary" type="submit"	variant="contained" >
                 Submit
             </Button>
