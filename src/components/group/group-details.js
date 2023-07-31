885de3051dda1345b2831b21e7d09b30816abb26
Abasp <abaspoor@hotmail.com>
2023-07-31 13:52:30 +0330

    npm i react-notification

--- src/components/group/group-details.js
+++ src/components/group/group-details.js
@@ -6,6 +6,9 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import { makeStyles} from "@mui/styles";
 import User from '../user/user';
+import {Button} from "@mui/material";
+import {JoinGroup,LeaveGroup} from '../../services/group-services';
+import {useAuth} from "../../hooks/useAuth";
 
 
 const useStyles = makeStyles(theme => ({
@@ -24,26 +27,48 @@ const useStyles = makeStyles(theme => ({
 
 function GroupDetails() {
     const classes = useStyles();
-
+	const {AuthD} = useAuth();
     const { id } = useParams();
     const [data , loading , error] = useFetchGroup(id);
 	const [ group , setGroup ] = useState(null);
+    const [inGroup,setInGroup] = useState(false);
+    const [isAdmin,setIsAdmin] = useState(false);
 
     useEffect(()=>{
+        if(data?.members){
+            if(AuthD?.user){
+                setInGroup(!!data.members.find(memeber => memeber.user.id === AuthD.user.id));
+                setIsAdmin(data.members.find(memeber => memeber.user.id === AuthD.user.id)?.admin);
+            }
+        }
 		setGroup(data);
     }, [data])
 
     if(error) return <h1>Error</h1>
     if(loading) return <h1>Loading....</h1>
 
-
+	const joinHere = () =>{
+		JoinGroup({user: AuthD.user.id, group: group.id}).then(res => {console.log(res);});
+        window.location.reload();
+    }
+    const leaveHere = () =>{
+        LeaveGroup({user: AuthD.user.id, group: group.id}).then(res => {console.log(res);});
+        window.location.reload();
+    }
     return (
         <div>
             <Link to={'/'}>Back</Link>
             { group &&
                 <React.Fragment>
-                <h1>{group.name} {group.location}</h1>
-				<h2>{group.description}</h2>
+                	<h1>{group.name} {group.location}</h1>
+					<h2>{group.description}</h2>
+                    {!inGroup ?
+                        <Button onClick={() => joinHere()} variant='contained' color='primary'>Join Group</Button>
+                        :
+                        <Button onClick={() => leaveHere()} variant='contained' color='primary'>leave Group</Button>
+                    }
+
+
                     <h3>Events:</h3>
                     {group.events.map (event => {
 						const format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
--- src/services/group-services.js
+++ src/services/group-services.js
@@ -9,3 +9,25 @@ export function getGroup(id){
     return fetch(`http://127.0.0.1:8000/api/groups/${id}/`)
         .then(status).catch(e => {console.log(e)})
 }
+
+export function JoinGroup(data){
+    return fetch(`http://127.0.0.1:8000/api/members/join/`,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(status).catch(e => {console.log(e)})
+}
+
+export function LeaveGroup(data){
+    return fetch(`http://127.0.0.1:8000/api/members/leave/`,{
+        method:'post',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(status).catch(e => {console.log(e)})
+}
